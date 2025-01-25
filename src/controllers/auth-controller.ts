e8d284b3ea0ceef30c84bcf2ca0d5e3d93fb034e
JuanNerySantos <juannnerysantos@gmail.com>
2025-01-25 19:05:12 -0300

    fix: bug in auth verb post

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
 import authService from "../service/auth-server";
-async function singup(res: Response, req: Request) {
-  const body = req.body;
-  const responseHttp = authService.singup(body);
 
+async function singup(req: Request, res: Response) {
+  const body = req.body;
+  const responseHttp = await authService.singup(body);
   res.send(responseHttp);
 }
 
