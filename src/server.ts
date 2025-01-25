9fae7fc55652eaf33129ef7152443bf53b84d53d
JuanNerySantos <juannnerysantos@gmail.com>
2025-01-24 23:23:27 -0300

    feat: create route from singup user

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
-import expres from "express";
+import expres, { json } from "express";
+import { authRouter } from "./routes/authRouter";
 
 const app = expres();
+
+app.use(json());
+
+app.use("/bank", authRouter);
 
 app.listen(3000, () => console.log("server listening in: localhost:3000"));
