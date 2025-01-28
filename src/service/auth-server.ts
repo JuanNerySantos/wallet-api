1cc64bfcd9949e928718c3520f5d5ede692b988a
JuanNerySantos <juannnerysantos@gmail.com>
2025-01-27 23:26:53 -0300

    feat: add lib bcrypt, convert passwort in hash

--- src/service/auth-server.ts
+++ src/service/auth-server.ts
@@ -1,9 +1,19 @@
+import bcrypt from "bcrypt";
 import { authModel } from "../models/auth-model";
 import authRepository from "../repositories/auth-repository";
 
 async function singup(auth: authModel) {
-  const createauth = authRepository.singup(auth);
-  return createauth;
+  if (Object.keys(auth).length === 0) {
+    throw new Error();
+  }
+  const hashPassword = bcrypt.hashSync(auth.password, 10);
+
+  const createAuth = await authRepository.singup({
+    ...auth,
+    password: hashPassword,
+  });
+
+  return createAuth;
 }
 
 export default { singup };
