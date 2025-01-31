8e0772a70d41c303ab59ac4dabef33a974d55036
JuanNerySantos <juannnerysantos@gmail.com>
2025-01-30 23:29:30 -0300

    refactor: Refactoring response add status code

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -2,9 +2,11 @@ import { Request, Response } from "express";
 import authService from "../service/auth-server";
 
 async function singup(req: Request, res: Response) {
-  const body = req.body;
-  const responseHttp = await authService.singup(body);
-  res.send(responseHttp);
+  const authParams = req.body;
+
+  const { statusCode, body } = await authService.singup(authParams);
+
+  res.status(statusCode).send(body);
 }
 
 export default { singup };
