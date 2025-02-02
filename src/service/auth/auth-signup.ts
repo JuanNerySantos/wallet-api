3e43f4a5e7fbe7492f08677a053d6de414f90314
JuanNerySantos <juannnerysantos@gmail.com>
2025-02-02 16:39:23 -0300

    refactor: creation of a single file for function authSignupService

---
+++ src/service/auth/auth-signup.ts
@@ -0,0 +1,26 @@
+import bcrypt from "bcrypt";
+import { authModel } from "../../models/auth-model";
+import { findAuthRepository } from "../../repositories/auth/find-auth";
+import { sigupRepository } from "../../repositories/auth/signup";
+import { badRquest, created } from "../http-response/http-response";
+
+export async function authSignupService(auth: authModel) {
+  if (Object.keys(auth).length === 0) {
+    throw new Error("Required params");
+  }
+
+  const authExist = await findAuthRepository(auth.email);
+
+  if (authExist) {
+    return badRquest({ message: "Email already exists!" });
+  }
+
+  const hashPassword = bcrypt.hashSync(auth.password, 10);
+
+  const createAuth = await sigupRepository({
+    ...auth,
+    password: hashPassword,
+  });
+
+  return created(createAuth);
+}
