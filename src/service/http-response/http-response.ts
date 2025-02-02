9fcf978fcb88fe319a00a44916a8308dc7595efe
JuanNerySantos <juannnerysantos@gmail.com>
2025-02-02 16:36:45 -0300

    feat: add function ok and add return the functions

--- src/service/http-response/http-response.ts
+++ src/service/http-response/http-response.ts
@@ -1,13 +1,29 @@
-export const created = (message: object) => {
+import { httpResponseModel } from "../../models/http-response";
+
+export const created = (message: object): httpResponseModel => {
   return {
     statusCode: 201,
     body: message,
   };
 };
 
-export const badRquest = (message: object) => {
+export const badRquest = (message: object): httpResponseModel => {
   return {
     statusCode: 400,
+    body: message,
+  };
+};
+
+export const unauthorized = (message: object): httpResponseModel => {
+  return {
+    statusCode: 401,
+    body: message,
+  };
+};
+
+export const ok = (message: object): httpResponseModel => {
+  return {
+    statusCode: 200,
     body: message,
   };
 };
