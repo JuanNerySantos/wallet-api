f89f0c8e813a7936f81943bfcb18196462ba4aec
JuanNerySantos <juannnerysantos@gmail.com>
2025-02-05 00:09:54 -0300

    feat: add route from signin

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { authSigninController } from "../controllers/auth/auth-signin";
 import { authSignupController } from "../controllers/auth/auth-signup";
 
 export const authRouter = Router();
 
 authRouter.post("/sigup", authSignupController);
-// authRouter.post("/signin", authSigninController);
+authRouter.post("/signin", authSigninController);
