64a8d96784fcb3a78b067edf69da9acb80073718
JuanNerySantos <juannnerysantos@gmail.com>
2025-02-07 02:15:19 -0300

    chore: use route transactions

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import expres, { json } from "express";
 import { connectMongoDb } from "./config/database";
 import { authRouter } from "./routes/authRouter";
+import { transactionRouter } from "./routes/transactionsRoutes";
 
 const app = expres();
 
@@ -10,6 +11,7 @@ app.use(json());
 connectMongoDb();
 
 app.use("/bank", authRouter);
+app.use("/bank", transactionRouter);
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`server listening in: localhost:${port}`));
