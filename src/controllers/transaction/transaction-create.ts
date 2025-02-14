d0d55459b2d33dc2978013d58851545fe5e122fb
JuanNerySantos <juannnerysantos@gmail.com>
2025-02-14 01:29:27 -0300

    feat: add required param id of the create transaction controller

--- src/controllers/transaction/transaction-create.ts
+++ src/controllers/transaction/transaction-create.ts
@@ -2,9 +2,9 @@ import { Request, Response } from "express";
 import { createTransactionService } from "../../service/transaction/transaction-create";
 export async function createTransactionController(req: Request, res: Response) {
   const transaction = req.body;
-  //   const id = "juan";
+  const { _id: id } = res.locals.auth;
 
-  const { statusCode, body } = await createTransactionService({ ...transaction });
+  const { statusCode, body } = await createTransactionService({ ...transaction }, id);
 
   res.status(statusCode).send(body);
 }
