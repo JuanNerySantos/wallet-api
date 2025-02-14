39da63ad8d08d0e457766d2a7a3e7afdff3f8894
JuanNerySantos <juannnerysantos@gmail.com>
2025-02-14 01:24:01 -0300

    feat: add return function create transaction repository

--- src/repositories/transaction/create-transaction.ts
+++ src/repositories/transaction/create-transaction.ts
@@ -1,6 +1,7 @@
 import { transactionModel } from "../../models/transaction-model";
 import TransactionSchema from "../../schemas/transaction";
-export async function createTransactionRepository(transaction: transactionModel) {
+
+export async function createTransactionRepository(transaction: transactionModel): Promise<transactionModel | {}> {
   const createTransaction = await TransactionSchema.create(transaction);
 
   return createTransaction;
