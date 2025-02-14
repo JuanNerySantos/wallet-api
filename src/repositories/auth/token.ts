684342f8d65fe08dda74137f2ce34e06825c3747
JuanNerySantos <juannnerysantos@gmail.com>
2025-02-14 01:20:35 -0300

    feat: add return function

--- src/repositories/auth/token.ts
+++ src/repositories/auth/token.ts
@@ -3,6 +3,6 @@ import jwt from "jsonwebtoken";
 
 const secret = process.env.SECRET as string;
 
-export async function tokenRepository(id: string) {
+export async function tokenRepository(id: string): Promise<String> {
   return jwt.sign({ id }, secret, { expiresIn: 86400 });
 }
