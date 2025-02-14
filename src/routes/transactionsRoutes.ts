e6eac6e7836d5a0c9ba0edb3f65ef88f495a6f46
JuanNerySantos <juannnerysantos@gmail.com>
2025-02-14 01:27:11 -0300

    fix: Correct the error in the return of the transaction route function

--- src/routes/transactionsRoutes.ts
+++ src/routes/transactionsRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { createTransactionController } from "../controllers/transaction/transaction-create";
+import { authMiddlewares } from "../middlewares/auth-middlewares";
 
 export const transactionRouter = Router();
 
-transactionRouter.post("/transaction", createTransactionController);
+transactionRouter.post("/transaction", authMiddlewares, createTransactionController);
