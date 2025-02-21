55f9fb80ee547ee6293d041475d00c1230335105
JuanNerySantos <juannnerysantos@gmail.com>
2025-02-21 00:44:53 -0300

    feat: add find transaction router

--- src/routes/transactionsRoutes.ts
+++ src/routes/transactionsRoutes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import { findTransactionController } from "../controllers/transaction/find-transaction";
 import { createTransactionController } from "../controllers/transaction/transaction-create";
 import { authMiddlewares } from "../middlewares/auth-middlewares";
 
 export const transactionRouter = Router();
 
 transactionRouter.post("/transaction", authMiddlewares, createTransactionController);
+transactionRouter.get("/transaction", authMiddlewares, findTransactionController);
