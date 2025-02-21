88e74d3551a3657ecceb3b8e8892e839d7bf1a87
JuanNerySantos <juannnerysantos@gmail.com>
2025-02-21 00:47:32 -0300

    fix: correct method to find all transactions

--- src/repositories/transaction/find-transaction.ts
+++ src/repositories/transaction/find-transaction.ts
@@ -2,7 +2,7 @@ import { transactionModel } from "../../models/transaction-model";
 import TransactionSchema from "../../schemas/transaction";
 
 export async function findTransactionRepository(id: string): Promise<transactionModel | {}> {
-  const transaction = await TransactionSchema.findById(id);
+  const transaction = await TransactionSchema.find({ userId: id });
   if (transaction) {
     return transaction;
   }
