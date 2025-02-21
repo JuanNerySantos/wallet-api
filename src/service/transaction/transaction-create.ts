62b05db7c8004153592eeb8a5be214c111198a0c
JuanNerySantos <juannnerysantos@gmail.com>
2025-02-20 23:48:38 -0300

    feat: add validations to the create transaction

--- src/service/transaction/transaction-create.ts
+++ src/service/transaction/transaction-create.ts
@@ -1,10 +1,25 @@
 import { httpResponseModel } from "../../models/http-response";
 import { transactionModel } from "../../models/transaction-model";
+import { findByIdRepository } from "../../repositories/auth/find-by-id";
 import { createTransactionRepository } from "../../repositories/transaction/create-transaction";
-import { ok } from "../http-response/http-response";
+import { badRquest, notFound, ok } from "../http-response/http-response";
 
-export async function createTransactionService(transaction: transactionModel): Promise<httpResponseModel> {
-  const createTransaction = await createTransactionRepository(transaction);
+export async function createTransactionService(transaction: transactionModel, id: string): Promise<httpResponseModel> {
+  if (!id) {
+    return badRquest({ message: "Id is required" });
+  }
+
+  if (Object.keys(transaction).length === 0) {
+    return badRquest({ message: "Params is required" });
+  }
+
+  const authExist = await findByIdRepository(id);
+
+  if (Object.keys(authExist).length === 0) {
+    return notFound({ message: "User not found." });
+  }
+
+  const createTransaction = await createTransactionRepository({ ...transaction, userId: id });
 
   return ok(createTransaction);
 }
