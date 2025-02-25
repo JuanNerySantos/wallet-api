7526dadf40812e1d6224cd6d8f7958a0fd51954f
JuanNerySantos <juannnerysantos@gmail.com>
2025-02-25 00:56:06 -0300

    feat: add auth logged router

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import { authLoggedController } from "../controllers/auth/auth-logged";
 import { authSigninController } from "../controllers/auth/auth-signin";
 import { authSignupController } from "../controllers/auth/auth-signup";
+import { authMiddlewares } from "../middlewares/auth-middlewares";
 
 export const authRouter = Router();
 
 authRouter.post("/sigup", authSignupController);
 authRouter.post("/signin", authSigninController);
+authRouter.get("/me", authMiddlewares, authLoggedController);
