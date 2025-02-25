624eb2faf73fadaa2f0b4bb9d4b6adc7d9ed6a40
JuanNerySantos <juannnerysantos@gmail.com>
2025-02-25 00:22:36 -0300

    feat: add validation in auth signin to verify if email and password are sent

--- src/service/auth/auth-signin.ts
+++ src/service/auth/auth-signin.ts
@@ -6,6 +6,10 @@ import { tokenRepository } from "../../repositories/auth/token";
 import { badRquest, ok, unauthorized } from "../http-response/http-response";
 
 export async function authSigninService(params: signinModel): Promise<httpResponseModel> {
+  const hhhh = ["juan", "nery"];
+  if (!["email", "password"].every(key => Object.keys(params).includes(key))) {
+    console.log("estamos chegando");
+  }
   if (!params.email) {
     return badRquest({ message: "password and email is requaired !" });
   }
