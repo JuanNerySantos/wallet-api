85ee10745cb65388fa3e4b0d0a82e3e47f1c7b02
JuanNerySantos <juannnerysantos@gmail.com>
2025-02-26 02:17:11 -0300

    feat: add joi to validate schema signin auth

---
+++ src/schemas/validation/signin-auth-user.ts
@@ -0,0 +1,8 @@
+import Joi from "joi";
+
+export const ValidationSigninAuthUser = Joi.object({
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  password: Joi.string().required().min(10),
+});
