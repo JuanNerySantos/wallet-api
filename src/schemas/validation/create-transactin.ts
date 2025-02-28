957bb7487f168b50e07a1521c9da943b09c275d8
JuanNerySantos <juannnerysantos@gmail.com>
2025-02-28 01:37:28 -0300

    fix: param type is string to schema create transaction

--- src/schemas/validation/create-transactin.ts
+++ src/schemas/validation/create-transactin.ts
@@ -3,7 +3,7 @@ import Joi from "joi";
 export const ValidationCreateTransaction = Joi.object({
   value: Joi.number().required(),
   description: Joi.number().required().min(3),
-  type: Joi.number().required().valid("input", "output"),
+  type: Joi.string().required().valid("input", "output"),
   userId: Joi.object(),
   createdAt: Joi.string(),
 });
