a66ee4bd79790e68b535f7f4129de71c649907cf
JuanNerySantos <juannnerysantos@gmail.com>
2025-02-28 01:43:10 -0300

    feat: add middleware to validation schemas to the create transaction

--- src/routes/transactionsRoutes.ts
+++ src/routes/transactionsRoutes.ts
@@ -2,10 +2,16 @@ import { Router } from "express";
 import { findTransactionController } from "../controllers/transaction/find-transaction";
 import { createTransactionController } from "../controllers/transaction/transaction-create";
 import { authMiddlewares } from "../middlewares/auth-middlewares";
+import { validationSchemaMiddlewares } from "../middlewares/schemas-middlewares";
+import { ValidationCreateTransaction } from "../schemas/validation/create-transactin";
 
 export const transactionRouter = Router();
 
 transactionRouter.use(authMiddlewares);
 
-transactionRouter.post("/transaction", createTransactionController);
+transactionRouter.post(
+  "/transaction",
+  validationSchemaMiddlewares(ValidationCreateTransaction),
+  createTransactionController
+);
 transactionRouter.get("/transaction", findTransactionController);
