80093d7b0f9787bc75591ed452cbfb5c68002132
JuanNerySantos <juannnerysantos@gmail.com>
2025-02-28 01:41:08 -0300

    feat: implement validantion the schema to create transaction

--- src/controllers/transaction/transaction-create.ts
+++ src/controllers/transaction/transaction-create.ts
@@ -1,14 +1,24 @@
 import { Request, Response } from "express";
 import { createTransactionService } from "../../service/transaction/transaction-create";
+import { ErrorMiddlewares } from "../error/middlewares-error";
 export async function createTransactionController(req: Request, res: Response) {
-  const transaction = req.body;
-  const { _id: id } = res.locals.auth;
-
   try {
+    if (res.locals.error !== undefined) {
+      throw new ErrorMiddlewares("error in validate middlewares");
+    }
+
+    const transaction = req.body;
+    const { _id: id } = res.locals.auth;
+
     const { statusCode, body } = await createTransactionService({ ...transaction }, id);
 
     res.status(statusCode).send(body);
   } catch (error) {
+    if (error instanceof ErrorMiddlewares) {
+      const existError = res.locals.error;
+      res.status(existError.statusCode).send(existError.message);
+      return;
+    }
     res.status(500).send("Internal server error!");
   }
 }
