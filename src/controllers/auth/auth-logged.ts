3bf4b7f49267417c695fbfbf5e6e37840d43b518
JuanNerySantos <juannnerysantos@gmail.com>
2025-02-28 02:01:13 -0300

    feat: add middleware to validation schemas to the find tarnsaction and auth logged

--- src/controllers/auth/auth-logged.ts
+++ src/controllers/auth/auth-logged.ts
@@ -1,14 +1,24 @@
 import { Request, Response } from "express";
 import { authLoggedService } from "../../service/auth/auth-logged";
+import { ErrorMiddlewares } from "../error/middlewares-error";
 
 export async function authLoggedController(req: Request, res: Response) {
-  const { _id: id } = res.locals.auth;
-
   try {
+    if (res.locals.error !== undefined) {
+      throw new ErrorMiddlewares("error in validate middlewares");
+    }
+
+    const { _id: id } = res.locals.auth;
+
     const { statusCode, body } = await authLoggedService(id);
 
     res.status(statusCode).send(body);
   } catch (error) {
+    if (error instanceof ErrorMiddlewares) {
+      const existError = res.locals.error;
+      res.status(existError.statusCode).send(existError.message);
+      return;
+    }
     res.status(500).send("Internal server error!");
   }
 }
--- src/controllers/transaction/find-transaction.ts
+++ src/controllers/transaction/find-transaction.ts
@@ -1,14 +1,23 @@
 import { Request, Response } from "express";
 import { findTransactionService } from "../../service/transaction/find-transaction";
+import { ErrorMiddlewares } from "../error/middlewares-error";
 
 export async function findTransactionController(req: Request, res: Response) {
-  const { _id: id } = res.locals.auth;
-
   try {
+    if (res.locals.error !== undefined) {
+      throw new ErrorMiddlewares("error in validate middlewares");
+    }
+    const { _id: id } = res.locals.auth;
+
     const { statusCode, body } = await findTransactionService(id);
 
     res.status(statusCode).send(body);
   } catch (error) {
+    if (error instanceof ErrorMiddlewares) {
+      const existError = res.locals.error;
+      res.status(existError.statusCode).send(existError.message);
+      return;
+    }
     res.status(500).send("Internal server error!");
   }
 }
