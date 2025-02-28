0406064d8d8924dfae348c4d07de9a6ba264dc19
JuanNerySantos <juannnerysantos@gmail.com>
2025-02-27 23:55:26 -0300

    feat: implement validantion the schema to signin

--- src/controllers/auth/auth-signin.ts
+++ src/controllers/auth/auth-signin.ts
@@ -1,14 +1,27 @@
 import { Request, Response } from "express";
 import { authSigninService } from "../../service/auth/auth-signin";
+import { ErrorMiddlewares } from "../error/middlewares-error";
 
-export async function authSigninController(req: Request, res: Response) {
+export async function authSigninController(req: Request, res: Response): Promise<void> {
   const params = req.body;
-
   try {
+    if (res.locals.error !== undefined) {
+      throw new ErrorMiddlewares("error in validate middlewares");
+    }
+
     const { statusCode, body } = await authSigninService(params);
 
     res.status(statusCode).send(body);
+    return;
   } catch (error) {
+    const existError = res.locals.error;
+    if (error instanceof ErrorMiddlewares) {
+      res.status(existError.statusCode).send(existError.message);
+      return;
+    }
+
+    console.error(error);
     res.status(500).send("Internal server error!");
+    return;
   }
 }
