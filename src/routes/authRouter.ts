716bd9da2851feba416b04942b6b560dba91eee2
JuanNerySantos <juannnerysantos@gmail.com>
2025-02-27 23:57:16 -0300

    feat: add middleware to validation schemas to the signin

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { authLoggedController } from "../controllers/auth/auth-logged";
 import { authSigninController } from "../controllers/auth/auth-signin";
-import { authSignupController } from "../controllers/auth/auth-signup";
 import { authMiddlewares } from "../middlewares/auth-middlewares";
+import { validationSchemaMiddlewares } from "../middlewares/schemas-middlewares";
+import { ValidationSigninAuthUser } from "../schemas/validation/signin-auth-user";
 
 export const authRouter = Router();
 
-authRouter.post("/sigup", authSignupController);
-authRouter.post("/signin", authSigninController);
+// authRouter.post("/sigup", validationSchemaMiddlewares(ValidationSignupAuthUser), authSignupController);
+authRouter.post("/signin", validationSchemaMiddlewares(ValidationSigninAuthUser), authSigninController);
 authRouter.get("/me", authMiddlewares, authLoggedController);
