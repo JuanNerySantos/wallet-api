1d36544885268060dff4561e7917073ecdda8103
JuanNerySantos <juannnerysantos@gmail.com>
2025-02-27 23:36:38 -0300

    refactor: removing redundant validations

--- src/service/auth/auth-signin.ts
+++ src/service/auth/auth-signin.ts
@@ -6,16 +6,6 @@ import { tokenRepository } from "../../repositories/auth/token";
 import { badRquest, ok, unauthorized } from "../http-response/http-response";
 
 export async function authSigninService(params: signinModel): Promise<httpResponseModel> {
-  if (!["email", "password"].every(key => Object.keys(params).includes(key))) {
-    console.log("estamos chegando");
-  }
-  if (!params.email) {
-    return badRquest({ message: "password and email is requaired !" });
-  }
-
-  if (!params.password) {
-    return badRquest({ message: "password and email is requaired !" });
-  }
   const authExist = await findAuthRepository(params.email);
 
   if ("password" in authExist) {
